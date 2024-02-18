3bf27cd26969dccd81ac4e86177d7d469cbe086e
inceisciberkay <inceisciberkay@gmail.com>
2024-02-18 18:55:52 +0300

    remove some todos

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -35,11 +35,11 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
+        // Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
         if (!basket.contains_key(&fruit)) {
-           basket.insert(fruit, 1);
+            basket.insert(fruit, 1);
         }
     }
 }
@@ -82,7 +82,7 @@ mod tests {
         let count = basket.values().sum::<u32>();
         assert!(count > 11);
     }
-    
+
     #[test]
     fn all_fruit_types_in_basket() {
         let mut basket = get_fruit_basket();
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -4,19 +4,19 @@
 // hint.
 
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
+    // Remove whitespace from both ends of a string!
     String::from(input.trim())
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
+    // Add " world!" to the string! There's multiple ways to do this!
     let mut composed = String::from(input);
     composed.push_str(" world!");
     composed
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
+    // Replace "cars" in the string with "balloons"!
     input.replace("cars", "balloons")
 }
 
